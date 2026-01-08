83cb330e1cc7c439ef8a8742d9666507a237fc77
Ubuntu <ubuntu@ip-172-31-42-95.ap-northeast-2.compute.internal>
2026-01-08 13:32:57 +0900

    update

---
+++ run_collect_prices.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export DJANGO_SETTINGS_MODULE=config.settings
+
+cd /home/ubuntu/pricehub
+source /home/ubuntu/pricehub/venv/bin/activate
+python /home/ubuntu/pricehub/collect_prices.py >> /home/ubuntu/pricehub/logs/collect_prices.log 2>&1
+
+echo "Completed at $(date)" >> /home/ubuntu/pricehub/logs/collect_prices.log
