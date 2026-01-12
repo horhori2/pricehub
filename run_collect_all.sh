027dd983368c33f339ca7506ac66323fef10af36
Ubuntu <ubuntu@ip-172-31-42-95.ap-northeast-2.compute.internal>
2026-01-12 13:41:19 +0900

    server

---
+++ run_collect_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export DJANGO_SETTINGS_MODULE=config.settings
+export CRON_MODE=true
+
+cd /home/ubuntu/pricehub
+source /home/ubuntu/pricehub/venv/bin/activate
+python /home/ubuntu/pricehub/collect_all_prices.py >> /home/ubuntu/pricehub/logs/collect_all_prices.log 2>&1
+
+echo "Completed at $(date)" >> /home/ubuntu/pricehub/logs/collect_all_prices.log
+
